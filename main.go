33393d1b3e19cf08be1029d8c92b5c2f040bc56c
Anish Mukherjee <mukherjee.an@northeastern.edu>
2020-08-21 14:25:14 -0400

    Playing multiple sounds at once/in order.

--- main.go
+++ main.go
@@ -26,6 +26,14 @@ func checkError(err error) {
 	}
 }
 
+// function to check if a file exists
+func checkIfExists(filepath string) {
+	_, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+}
+
 // function to open an audio file and return a streamer
 // needs to be provided a filepath
 func getStreamer(filepath string) (beep.StreamSeekCloser, beep.Format) {
@@ -42,7 +50,20 @@ func getStreamer(filepath string) (beep.StreamSeekCloser, beep.Format) {
 // function to play an audio file given the filepath and a pre-initialized speaker
 func playAudio(filepath string) {
 	streamer, _ := getStreamer(filepath)
+	// play the sound
 	speaker.Play(streamer)
+	streamer.Seek(0)
+}
+
+// function to return an array of streamers based on their filepaths
+func getStreamers(filepaths ...string) []beep.StreamSeekCloser {
+	streamerArray := make([]beep.StreamSeekCloser, len(filepaths))
+	for i := 0; i < len(streamerArray); i++ {
+		checkIfExists(filepaths[i])
+		streamer, _ := getStreamer(filepaths[i])
+		streamerArray[i] = streamer
+	}
+	return streamerArray
 }
 
 func main() {
@@ -67,16 +88,30 @@ func main() {
 		}
 	})
 
-	// initialize the speaker with the sample rate and buffer size with one of the samples in the library
-	_, format := getStreamer("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
-
 	// what to do on the callme page
 	http.HandleFunc("/callme", func(w http.ResponseWriter, r *http.Request) {
+		// initialize the speaker with the sample rate and buffer size with one of the samples in the library
+		_, format := getStreamer("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
 		// play the audio corresponding to the filepath
 		playAudio("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
 		// log the action
 		fmt.Println("Play Audio")
+	})
+
+	// multiple noises played after each other
+	http.HandleFunc("/multiNoise", func(w http.ResponseWriter, r *http.Request) {
+		// initialize the speaker with the sample rate and buffer size with one of the samples in the library
+		_, format := getStreamer("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+		// get all of the streamers necessary
+		streamerArray := getStreamers("static/audio/drums/kick/kick.wav",
+			"static/audio/drums/snare/snare.wav", "static/audio/drums/kick/kick.wav", "static/audio/drums/snare/snare.wav")
+		// pattern of a drum and a snare for a measure
+		// (kick snare kick snare)
+		speaker.Play(beep.Seq(streamerArray[0], streamerArray[1], streamerArray[2], streamerArray[3]))
 	})
 
 	// start the server, open the port to 4200, without a path it assumes localhost
