a9db7b26233d7f9e6244f4eba4603c6999fee15b
Anish Mukherjee <mukherjee.an@northeastern.edu>
2020-08-25 16:20:47 -0400

    Able to create a sequence of drums and play them. Need to be able to choose sample sounds next.

--- main.go
+++ main.go
@@ -19,6 +19,49 @@ type Welcome struct {
 	Name string
 }
 
+// type to hold a queue of drum samples
+type Queue struct {
+	streamers []beep.StreamSeekCloser
+}
+
+// function to add something to the queue of streamers
+func (q *Queue) Add(streamers []beep.StreamSeekCloser) {
+	q.streamers = append(q.streamers, streamers...)
+}
+
+// function to stream all of the samples in a queue
+func (q *Queue) Stream(samples [][2]float64) (n int, ok bool) {
+	// We use the filled variable to track how many samples we've
+	// successfully filled already. We loop until all samples are filled.
+	filled := 0
+	for filled < len(samples) {
+		// There are no streamers in the queue, so we stream silence.
+		if len(q.streamers) == 0 {
+			for i := range samples[filled:] {
+				samples[i][0] = 0
+				samples[i][1] = 0
+			}
+			break
+		}
+
+		// We stream from the first streamer in the queue.
+		n, ok := q.streamers[0].Stream(samples[filled:])
+		// If it's drained, we pop it from the queue, thus continuing with
+		// the next streamer.
+		if !ok {
+			q.streamers = q.streamers[1:]
+		}
+		// We update the number of filled samples.
+		filled += n
+	}
+	return len(samples), true
+}
+
+// Err: trivial error function for queues
+func (q *Queue) Err() error {
+	return nil
+}
+
 // function to log errors
 func checkError(err error) {
 	if err != nil {
@@ -108,10 +151,52 @@ func main() {
 
 		// get all of the streamers necessary
 		streamerArray := getStreamers("static/audio/drums/kick/kick.wav",
-			"static/audio/drums/snare/snare.wav", "static/audio/drums/kick/kick.wav", "static/audio/drums/snare/snare.wav")
+			"static/audio/drums/snare/snare909.wav", "static/audio/drums/kick/kick.wav", "static/audio/drums/snare/snare909.wav")
 		// pattern of a drum and a snare for a measure
 		// (kick snare kick snare)
-		speaker.Play(beep.Seq(streamerArray[0], streamerArray[1], streamerArray[2], streamerArray[3]))
+
+		// add them to a queue and play the queue
+		var queue Queue
+		queue.Add(streamerArray)
+		speaker.Play(&queue)
+	})
+
+	drumsSelected := []string{}
+
+	http.HandleFunc("/fillForm", func(w http.ResponseWriter, r *http.Request) {
+		// if there is an error while executing the home page template, print it
+		if err := templates.ExecuteTemplate(w, "home-template.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		r.ParseForm()
+		fmt.Printf("%+v\n", r.Form)
+		drumsSelected = append(drumsSelected, r.Form["drums"]...)
+		// initialize the speaker with the sample rate and buffer size with one of the samples in the library
+		_, format := getStreamer("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+		// get the corresponding streamers
+		streamerArray := getStreamers(drumsSelected...)
+
+		// add them to a queue depending on which checkboxes are ticked and play the queue
+		var queue Queue
+		queue.Add(streamerArray)
+		speaker.Play(&queue)
+	})
+
+	// playing a drum pattern based on the checkboxes ticked
+	http.HandleFunc("/playDrums", func(w http.ResponseWriter, r *http.Request) {
+		// initialize the speaker with the sample rate and buffer size with one of the samples in the library
+		_, format := getStreamer("static/audio/Alesis-Fusion-Tubular-Bells-C6.wav")
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+		// get the corresponding streamers
+		streamerArray := getStreamers(drumsSelected...)
+
+		// add them to a queue depending on which checkboxes are ticked and play the queue
+		var queue Queue
+		queue.Add(streamerArray)
+		speaker.Play(&queue)
 	})
 
 	// start the server, open the port to 4200, without a path it assumes localhost
